fd318bba98d3d8038e1b2996db36b8d2a1e9d5cc
Beaskyy <afeezakintobi@nqb8.co>
2023-10-19 09:48:06 +0100

    form input

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { Modal } from "@/components/ui/modal";
 import { useStoreModal } from "@/hooks/use-store-modal";
-import { UserButton } from "@clerk/nextjs";
 import { useEffect } from "react";
 
 const SetupPage = () => {
