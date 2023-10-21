ac7b179010b62061fed48479bddc7c305b5ebfcd
Beaskyy <afeezakintobi@nqb8.co>
2023-10-21 21:33:20 +0100

    navbar

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -1,7 +1,16 @@
+import prismadb from "@/lib/prismadb"
 
-const DashboardPage = () => {
+interface DashboardPageProps {
+  params: {storeId: string}
+}
+const DashboardPage: React.FC<DashboardPageProps> = async ({params}) => {
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: params.storeId
+    }
+  })
   return (
-    <div>This is a Dashboard!</div>
+    <div>Active Store : {store?.name}</div>
   )
 }
 
